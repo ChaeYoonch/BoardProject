96f3dd4e4e4c91a4c290631809bcdbd710a5146e
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-25 16:32:41 +0900

    AlertRedirectException : 페이지 관련 추가 정보

--- src/main/java/org/choongang/global/exceptions/AlertRedirectException.java
+++ src/main/java/org/choongang/global/exceptions/AlertRedirectException.java
@@ -3,13 +3,23 @@ package org.choongang.global.exceptions;
 public class AlertRedirectException extends AlertException {
 
     private String redirectUrl;
+    private String target;
 
-    public AlertRedirectException(String message, String redirectUrl, int status) {
+    public AlertRedirectException(String message, String redirectUrl, int status, String target) {
         super(message, status);
         this.redirectUrl = redirectUrl;
+        this.target = target;
+    }
+
+    public AlertRedirectException(String message, String redirectUrl, int status) {
+        this(message, redirectUrl, status, "self"); // 현재 창에서 이동할 수 있도록 (다르 창이 생성되지 않도록 설정)
     }
 
     public String getRedirectUrl() {
         return redirectUrl;
+    }
+
+    public String getTarget() {
+        return target; // 미입력 시 self 로 고정됨
     }
 }
