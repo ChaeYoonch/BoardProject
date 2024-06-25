97dbe94840fdc2ba82d0c88a2911c81751755c1a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-25 15:38:01 +0900

    RequestLogin : 로그인 데이터

--- src/main/java/org/choongang/member/controllers/RequestLogin.java
+++ src/main/java/org/choongang/member/controllers/RequestLogin.java
@@ -3,7 +3,7 @@ package org.choongang.member.controllers;
 import lombok.Data;
 
 @Data
-public class RequestLogin {
+public class RequestLogin { // 로그인 데이터
     private String email;
     private String password;
     private boolean saveEmail;
