4d8cde479fc4264d4d1a7c9f42017330bb8f3c11
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-25 10:31:44 +0900

    MemberController

---
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -0,0 +1,39 @@
+package org.choongang.member.controllers;
+
+import org.choongang.global.config.annotations.Controller;
+import org.choongang.global.config.annotations.GetMapping;
+import org.choongang.global.config.annotations.PostMapping;
+import org.choongang.global.config.annotations.RequestMapping;
+
+@Controller
+@RequestMapping("/member") // 맨 앞의 주소는 member 로 매칭됨
+public class MemberController {
+
+    // 회원가입 양식
+    @GetMapping("/join") // member/join 으로 유입됨
+    public String join() {
+
+        return "member/join"; // join.jsp 로 이동
+    }
+
+    // 회원가입 처리
+    @PostMapping("/join")
+    public String joinPs() {
+
+        return null;
+    }
+
+    // 로그인 양식
+    @GetMapping("/login") // member/login 으로 유입됨
+    public String login() {
+
+        return "member/login"; // login.jsp 로 이동
+    }
+
+    // 로그인 처리
+    @PostMapping("/login")
+    public String loginPs() {
+
+        return null;
+    }
+}
