24b3443ff49f1f5911421819de7258bb35b20875
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-01 11:04:47 +0900

    MemberUtil

--- src/main/java/org/choongang/member/MemberUtil.java
+++ src/main/java/org/choongang/member/MemberUtil.java
@@ -1,38 +1,37 @@
 package org.choongang.member;
 
 import jakarta.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Component;
 import org.choongang.global.config.containers.BeanContainer;
 import org.choongang.member.constants.UserType;
 import org.choongang.member.entities.Member;
 
 @Component
-@RequiredArgsConstructor
 public class MemberUtil {
-
     // 로그인 여부
     public boolean isLogin() {
-        return getMember() != null; // 값이 있으면 로그인 상태 O
+        return getMember() != null;
     }
 
     // 관리자 여부
     public boolean isAdmin() {
         if (isLogin()) {
-            Member member = getMember(); // login 상태 O
+            Member member = getMember();
 
-            return member.getUserType() == UserType.ADMIN; // UserType 이 ADMIN 인 경우
+            return member.getUserType() == UserType.ADMIN;
         }
-
         return false;
     }
 
     /**
-     * 로그인한 회원 정보 | 조회가 되면 로그인 상태
+     * 로그인한 회원 정보
      * @return
      */
     public Member getMember() {
         HttpSession session = BeanContainer.getInstance().getBean(HttpSession.class);
+        if (session == null) {
+            return null;
+        }
         Member member = (Member)session.getAttribute("member");
 
         return member;
